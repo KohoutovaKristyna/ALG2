4a18c3a1333458a14b1887b97cefd6086b272087
kristyna.kohoutova@tul.cz <61623629+KohoutovaKristyna@users.noreply.github.com>
2020-05-09 19:03:15 +0200

    update

--- Kohoutova_ALG2_sem_Autoskola/src/ui/Main.java
+++ Kohoutova_ALG2_sem_Autoskola/src/ui/Main.java
@@ -5,6 +5,8 @@
  */
 package ui;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kristyna kohoutova
@@ -15,7 +17,41 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //declaration
+        Scanner sc = new Scanner(System.in);
+        boolean keepGoing = true;
+        String userAnswer;
+        boolean wrongAnswer;
+
+        //the body of program
+        while (keepGoing) {
+            
+            
+            
+            
+            
+            //find out if user wants to continue
+            System.out.println("Chcete pokracovat? a/n");
+            userAnswer = sc.nextLine();
+            if (userAnswer.toLowerCase().equals("a")) {
+
+            } else if (userAnswer.toLowerCase().equals("n")) {
+                keepGoing = false;
+            } else {           // if user enter something else it will not let him continue or end
+                wrongAnswer = false;
+                while (wrongAnswer == false) {
+                    System.out.println("Spatna odpoved, zkuste to znovu");
+                    userAnswer = sc.nextLine();
+                    if (userAnswer.toLowerCase().equals("a") || userAnswer.toLowerCase().equals("n")) {
+                        wrongAnswer = true;
+                        if (userAnswer.toLowerCase().equals("a")) {
+
+                        } else if (userAnswer.toLowerCase().equals("n")) {
+                            keepGoing = false;
+                        }
+                    }
+                }
+            }
+        }
     }
-    
 }
